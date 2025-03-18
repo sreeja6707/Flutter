78b663a30d8946b0291f785691ce9acb0211692f
sreeja6707 <153908355+sreeja6707@users.noreply.github.com>
2025-03-18 19:57:17 +0530

    Update switch_demo_lib.dart

--- switch_demo_lib.dart
+++ switch_demo_lib.dart
@@ -1,1 +1,30 @@
+String describeNumber(int number) {
+  return switch (number) {
+    >= 90 => 'A',
+    >= 80 => 'B',
+    >= 70 => 'C',
+    >= 60 => 'D',
+    _ => 'Fail',
+  };
+}
 
+
+
+
+
+//  String describeNumber(int number) {
+//    switch (number) {
+//      case 1:
+//        return "Apple";
+//        break;
+//      case 2:
+//        return "Banana";
+//        break;
+//      case 3:
+//        return "Carrot";
+//        break;
+//      case _:
+//        return "Other";
+//        break;
+//    }
+//  }
