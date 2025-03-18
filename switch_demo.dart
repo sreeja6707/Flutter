789d345efdf6cd9ebdded01d0df8d9a76d23dd28
sreeja6707 <153908355+sreeja6707@users.noreply.github.com>
2025-03-18 19:58:02 +0530

    Add files via upload

--- switch_demo.dart
+++ switch_demo.dart
@@ -1,17 +1,25 @@
-String describeNumber(int number) {
-  return switch (number) {
-    >= 90 => 'A',
-    >= 80 => 'B',
-    >= 70 => 'C',
-    >= 60 => 'D',
-    _ => 'Fail',
-  };
-}
+import 'package:switch_demo/switch_demo.dart' as switch_demo;
+//Simple Switch[
+//void main(List<String> arguments) {
+//String describeNumber(int number) {
+//switch (number) {
+// case 1:
+//    return "Apple";
+//  case 2:
+//    return "Banana";
+//  case 3:
+//    return "Carrot";
+//  default:
+//    return "Other";
+//}
+//}
 
+//print('Hello world: ${switch_demo.describeNumber(1)}!');
+//}
+//]
 
-
-
-
+//WITH CASE_(
+//void main(List<String> arguments) {
 //  String describeNumber(int number) {
 //    switch (number) {
 //      case 1:
@@ -28,3 +36,36 @@ String describeNumber(int number) {
 //        break;
 //    }
 //  }
+
+//  print('Hello world: ${switch_demo.describeNumber(2)}!');
+//}
+//)
+
+//SWITCH RETURN
+//void main(List<String> arguments) {
+//  String describeNumber(int number) {
+//    return switch (number) {
+//      >= 90 => 'A',
+//      >= 80 => 'B',
+//      >= 70 => 'C',
+//      >= 60 => 'D',
+//      _ => 'Fail',
+//    };
+//  }
+
+//  print('Hello world: ${switch_demo.describeNumber(80)}!');
+//}
+void main(List<String> arguments) {
+  String describeNumber(int number) {
+    var x = switch (number) {
+      >= 90 => 'A',
+      >= 80 => 'B',
+      >= 70 => 'C',
+      >= 60 => 'D',
+      _ => 'Fail',
+    };
+    return x;
+  }
+
+  print('Hello world: ${switch_demo.describeNumber(80)}!');
+}
